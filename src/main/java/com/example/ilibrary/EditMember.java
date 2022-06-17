b01d6741694a8f133664693e017b761ee9ba4c4e
eunannana <thisiseunkim93@gmail.com>
2022-06-17 23:53:52 +0700

    update layout

---
+++ src/main/java/com/example/ilibrary/EditMember.java
@@ -0,0 +1,14 @@
+package com.example.ilibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_member);
+    }
+}
